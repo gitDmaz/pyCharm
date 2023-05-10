c2f2b76d9596ac2138ff61bc480166e118a2056e
Debdatta Mazumder <mazumder520deb@gmail.com>
2023-05-10 07:21:01 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,9 @@
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the time period (in years): "))
+
+# Calculating Simple Interest
+simple_interest = (principal * rate * time) / 100
+
+# Printing the Simple Interest
+print("Simple Interest = ", simple_interest)
